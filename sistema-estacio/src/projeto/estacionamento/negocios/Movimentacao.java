553b54dccd38bace001ddf34a0c6916cdf164c26
morfeu9 <gabriel_nogueira21@hotmail.com>
2019-06-23 11:29:27 -0300

    Add GaS nas classes e alterando a vaga para static

--- sistema-estacio/src/projeto/estacionamento/negocios/Movimentacao.java
+++ sistema-estacio/src/projeto/estacionamento/negocios/Movimentacao.java
@@ -12,4 +12,41 @@ public class Movimentacao {
     private LocalDateTime dataHoraEntrada;
     private LocalDateTime dataHoraSaida;
     private double valor;
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public LocalDateTime getDataHoraEntrada() {
+        return dataHoraEntrada;
+    }
+
+    public void setDataHoraEntrada(LocalDateTime dataHoraEntrada) {
+        this.dataHoraEntrada = dataHoraEntrada;
+    }
+
+    public LocalDateTime getDataHoraSaida() {
+        return dataHoraSaida;
+    }
+
+    public void setDataHoraSaida(LocalDateTime dataHoraSaida) {
+        this.dataHoraSaida = dataHoraSaida;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Movimentacao(Veiculo veiculo, LocalDateTime dataHoraEntrada) {
+        this.veiculo = veiculo;
+        this.dataHoraEntrada = dataHoraEntrada;
+    }
 }
--- sistema-estacio/src/projeto/estacionamento/negocios/Vaga.java
+++ sistema-estacio/src/projeto/estacionamento/negocios/Vaga.java
@@ -8,7 +8,9 @@ package projeto.estacionamento.negocios;
 public class Vaga {
 
     public static int total_vagas = 100;
-    private int vagasOcupadas = 0;
+    private static int vagasOcupadas = 0;
+
+    private Vaga(){}
 
     /**
      * Verificar se ha alguma vaga livre
@@ -17,5 +19,35 @@ public class Vaga {
     public boolean vagaLivre(){
         //TODO  implementar
         return false;
+    }
+
+    /**
+     * Buscar os dados atuais quando inicializar o sistema
+     */
+    public static void inicializarOcupadas(){
+        //TODO implementar
+    }
+
+    /**
+     * retorna o numero de vagas ocupadas
+     * @return o numero total de vagas ocupadas quando necessario
+     */
+    public static int ocupadas(){
+        return Vaga.vagasOcupadas;
+    }
+
+    /**
+     * retorna o numero de vagas livres
+     * @return o numero total de vagas disponiveis quando necessario
+     */
+    public static int livres(){
+        return total_vagas - Vaga.vagasOcupadas;
+    }
+
+    /**
+     * atualiza o numero de vagas ocupadas apos entrada de veiculos
+     */
+    public static void entrou(){
+        Vaga.vagasOcupadas ++;
     }
 }
